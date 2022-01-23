449ea607dd9d4fb3d3dc6e40085c6259d3daca5b
Sanskar Modi <modisanskar5@gmail.com>
2022-01-24 00:27:06 +0530

    First Page completed

--- lib/UI/first_page.dart
+++ lib/UI/first_page.dart
@@ -1,81 +1,241 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:profile_parsing/Provider/provider.dart';
+import 'package:profile_parsing/UI/second_page.dart';
 import 'package:provider/provider.dart';
 
-class ProviderWidget extends StatelessWidget {
-  final textSize = 30;
-  final counterModel = CounterModel();
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+class _FirstPageState extends State<FirstPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animation;
+TextEditingController? name = TextEditingController();
+TextEditingController? email=TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: CounterModel(),
-      child: FirstPage(),
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
     );
-  }
-}
 
-class CounterModel with ChangeNotifier {
-  int _countNum = 0;
-
-  int get countNum => _countNum;
-
-  void increment() {
-    _countNum++;
-    notifyListeners();
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final _counter = Provider.of<CounterModel>(context);
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Show ${_counter._countNum}'),
-          RaisedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => ChangeNotifierProvider.value(
-                    value: _counter,
-                    child: SecondPage(),
-                  ),
-                ),
-              );
-            },
-            child: Text('Goto SecondPage'),
-          )
-        ],
+    _animation = Tween<double>(begin: .7, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.ease,
       ),
+    )..addListener(
+          () {
+        setState(() {});
+      },
+    )..addStatusListener(
+          (status) {
+        if (status == AnimationStatus.completed) {
+          _controller!.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _controller!.forward();
+        }
+      },
     );
+
+    _controller!.forward();
   }
-}
 
-class SecondPage extends StatefulWidget {
   @override
-  _SecondPageState createState() => _SecondPageState();
-}
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
 
-class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     var counter = Provider.of<CounterModel>(context);
-
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('SecondPage'),
-      ),
-      body: Container(
-        child: Center(
-          child: RaisedButton(
-            onPressed: () => counter.increment(),
-            child: Text('Add'),
+      backgroundColor: Color(0xff292C31),
+      body: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: _height,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(),
+                      Text(
+                        'SIGN IN',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xffA9DED8),
+                        ),
+                      ),
+                      SizedBox(),
+                      component1(Icons.account_circle_outlined, 'User name...',
+                          false),
+                      component2(Icons.email_outlined, 'Email...', false),
+
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Stack(
+                    children: [
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.only(bottom: _width * .07),
+                          height: _width * .7,
+                          width: _width * .7,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.transparent,
+                                Colors.transparent,
+                                Color(0xff09090A),
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Transform.scale(
+                          scale: _animation!.value,
+                          child: InkWell(
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            onTap: () {
+                              HapticFeedback.lightImpact();
+                              Fluttertoast.showToast(
+                                msg: 'SIGN-IN button pressed',
+                              );
+                              counter.Data(name!.text, email!.text);
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => ChangeNotifierProvider.value(
+                                    value: counter,
+                                    child: SecondPage(),
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              height: _width * .2,
+                              width: _width * .2,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Color(0xffA9DED8),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Text(
+                                'SIGN-IN',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget component1(
+      IconData icon, String hintText,  bool isEmail) {
+    double _width = MediaQuery.of(context).size.width;
+    return Container(
+      height: _width / 8,
+      width: _width / 1.22,
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(right: _width / 30),
+      decoration: BoxDecoration(
+        color: Color(0xff212428),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: TextField(
+        controller:name,
+        style: TextStyle(color: Colors.white.withOpacity(.9)),
+
+        keyboardType:  TextInputType.text,
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            icon,
+            color: Colors.white.withOpacity(.7),
+          ),
+          border: InputBorder.none,
+          hintMaxLines: 1,
+          hintText: hintText,
+          hintStyle: TextStyle(
+            fontSize: 14,
+            color: Colors.white.withOpacity(.5),
+          ),
+        ),
+      ),
+    );
+  }
+  Widget component2(
+      IconData icon, String hintText,  bool isEmail) {
+    double _width = MediaQuery.of(context).size.width;
+    return Container(
+      height: _width / 8,
+      width: _width / 1.22,
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(right: _width / 30),
+      decoration: BoxDecoration(
+        color: Color(0xff212428),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: TextField(
+        controller:email,
+        style: TextStyle(color: Colors.white.withOpacity(.9)),
+
+        keyboardType:TextInputType.emailAddress ,
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            icon,
+            color: Colors.white.withOpacity(.7),
+          ),
+          border: InputBorder.none,
+          hintMaxLines: 1,
+          hintText: hintText,
+          hintStyle: TextStyle(
+            fontSize: 14,
+            color: Colors.white.withOpacity(.5),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
